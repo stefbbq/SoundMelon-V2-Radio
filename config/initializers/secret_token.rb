66d111f824812881896dedd36c51a5888eac175b
adelamodwala <adel.amodwala@gmail.com>
2013-03-22 22:03:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundMelonV2Radio::Application.config.secret_token = '682bf6cdb0a2f3d523970d0937e57ae200a41e5bff84eebef93ba7523c2edb0d04097d1d4e4c7044944f407cb67e12b5e2602ec2f061f0bb56f305bf3cb179d5'
