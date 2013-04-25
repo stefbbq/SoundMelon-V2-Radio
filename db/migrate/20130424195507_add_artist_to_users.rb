2324cc021721deaa28f6265058573f89c5c13dc6
adelamodwala <adel.amodwala@gmail.com>
2013-04-25 18:35:35 -0400

    Modify database for artists again

--- db/migrate/20130424195507_add_artist_to_users.rb
+++ db/migrate/20130424195507_add_artist_to_users.rb
@@ -1,5 +1,6 @@
 class AddArtistToUsers < ActiveRecord::Migration
   def change
 		add_column :users, :is_artist, :boolean, default: false
+		add_column :users, :artist_id, :integer, references: 'artists'
   end
 end
