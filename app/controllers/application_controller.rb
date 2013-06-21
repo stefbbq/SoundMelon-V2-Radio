8f3cba75b2cb85aa10d0c1bb00321cf3095d2c2a
Adel <adel@PintsizeX.(none)>
2013-06-21 15:38:07 -0400

    More youtube_it config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 	include SessionsHelper
 
-	$localtunnel_add = 'http://46g3.localtunnel.com'
+	$localtunnel_add = 'http://localhost:3000'
   # Force signout to prevent CSRF attacks
   def handle_unverified_request
     sign_out
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,9 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user 	#makes the variable 'current_user' 
 														 	#available in all views and controllers
+		if current_user.is_artist?
+			self.current_artist = Artist.find(current_user.artist_id)
+		end
   end
 
   def signed_in?
@@ -17,6 +20,16 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
+
+	def current_artist=(artist)
+    @current_artist = artist
+  end
+  
+  def current_artist
+		if current_user.is_artist?
+    	@current_artist ||= Artist.find_by_id(current_user.artist_id)
+  	end
+	end
 
   def sign_out
     self.current_user = nil
