f882665e137a5fee04e03acb9aae9bd2e3fe2169
Adel Amodwala <adel.amodwala@gmail.com>
2014-08-04 13:13:51 -0400

    Add GA

--- app/assets/javascripts/SMhome.js
+++ app/assets/javascripts/SMhome.js
@@ -4,7 +4,15 @@ $(document).ready(function() {
 	FlashManager = new SMflashManager();
 	ModalManager = new SMmodalManager();
 	
-//Add AJAX-loaders
-$(document).ajaxStop(spinnerAjaxStop);
+	//Add AJAX-loaders
+	$(document).ajaxStop(spinnerAjaxStop);
+
+	//setup GA events
+	$("#learn-more a").click(function() {
+		ga('send', 'event', 'button', 'click', 'about-soundmelon');
+	});
+	$("#sign-in a").click(function() {
+		ga('send', 'event', 'button', 'click', 'signin-signup');
+	});
 
 });
