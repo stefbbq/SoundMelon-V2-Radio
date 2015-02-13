747203675705e67d2fba76cf02950ea9982b4734
Adel Amodwala <adel.amodwala@gmail.com>
2015-02-12 19:25:56 -0500

    Fix sharing, add basic tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save without basic info" do
+  	# Basic information is email, email_confirmation, and password
+  	user = User.new
+  	assert !user.save, "Saved the user without basic information"
+	end
+
+	test "should not save with mismatched emails" do
+		user = User.new(email: "abe@gabe.com", email_confirmation: "abe@gab.com")
+		assert !user.save, "Saved the user mismatched emails"
+	end
+
 end
