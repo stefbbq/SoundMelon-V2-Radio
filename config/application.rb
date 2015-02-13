6ec4a70d9d54df61e864948afe8255eea2a6da19
Adel Amodwala <adel.amodwala@gmail.com>
2015-02-12 20:26:22 -0500

    Add local_evn.yml for production

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,15 @@ module SoundMelonV2Radio
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-		# Extending core classes (e.g. String, Hash, etc) for the app
-		config.autoload_paths += Dir[File.join(Rails.root, "lib", "core_ext", "*.rb")].each {|l| require l }
+	# Extending core classes (e.g. String, Hash, etc) for the app
+	config.autoload_paths += Dir[File.join(Rails.root, "lib", "core_ext", "*.rb")].each {|l| require l }
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+    
   end
 end
